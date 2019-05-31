0b971897d16685735568816ae224597a776cab86
Doug Gish <doug.gish@ge.com>
2019-05-31 10:27:22 -0600

    Allow message interceptors to be scoped services

--- IO.Eventuate.Tram/Consumer/Common/PrePostHandlerMessageHandlerDecorator.cs
+++ IO.Eventuate.Tram/Consumer/Common/PrePostHandlerMessageHandlerDecorator.cs
@@ -2,48 +2,44 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using IO.Eventuate.Tram.Messaging.Common;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace IO.Eventuate.Tram.Consumer.Common
 {
 	public class PrePostHandlerMessageHandlerDecorator : IMessageHandlerDecorator, IOrdered
 	{
-		private readonly IMessageInterceptor[] _messageInterceptors;
-
-		public PrePostHandlerMessageHandlerDecorator(IEnumerable<IMessageInterceptor> messageInterceptors)
-		{
-			_messageInterceptors = messageInterceptors.ToArray();
-		}
-
 		public Action<SubscriberIdAndMessage, IServiceProvider, IMessageHandlerDecoratorChain> Accept =>
 			(subscriberIdAndMessage, serviceProvider, messageHandlerDecoratorChain) =>
 			{
 				IMessage message = subscriberIdAndMessage.Message;
 				string subscriberId = subscriberIdAndMessage.SubscriberId;
-				PreHandle(subscriberId, message);
+				IMessageInterceptor[] messageInterceptors =
+					serviceProvider.GetServices<IMessageInterceptor>().ToArray();
+				PreHandle(subscriberId, message, messageInterceptors);
 				try
 				{
 					messageHandlerDecoratorChain.InvokeNext(subscriberIdAndMessage, serviceProvider);
-					PostHandle(subscriberId, message, null);
+					PostHandle(subscriberId, message, messageInterceptors, null);
 				}
 				catch (Exception e)
 				{
-					PostHandle(subscriberId, message, e);
+					PostHandle(subscriberId, message, messageInterceptors, e);
 					throw;
 				}
 			};
 
-		private void PreHandle(string subscriberId, IMessage message)
+		private void PreHandle(string subscriberId, IMessage message, IEnumerable<IMessageInterceptor> messageInterceptors)
 		{
-			foreach (IMessageInterceptor messageInterceptor in _messageInterceptors)
+			foreach (IMessageInterceptor messageInterceptor in messageInterceptors)
 			{
 				messageInterceptor.PreHandle(subscriberId, message);
 			}
 		}
 
 
-		private void PostHandle(string subscriberId, IMessage message, Exception e)
+		private void PostHandle(string subscriberId, IMessage message, IEnumerable<IMessageInterceptor> messageInterceptors, Exception e)
 		{
-			foreach (IMessageInterceptor messageInterceptor in _messageInterceptors)
+			foreach (IMessageInterceptor messageInterceptor in messageInterceptors)
 			{
 				messageInterceptor.PostHandle(subscriberId, message, e);
 			}
--- IO.Eventuate.Tram/Consumer/Common/PrePostReceiveMessageHandlerDecorator.cs
+++ IO.Eventuate.Tram/Consumer/Common/PrePostReceiveMessageHandlerDecorator.cs
@@ -2,46 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using IO.Eventuate.Tram.Messaging.Common;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace IO.Eventuate.Tram.Consumer.Common
 {
 	public class PrePostReceiveMessageHandlerDecorator : IMessageHandlerDecorator, IOrdered
 	{
-
-		private readonly IMessageInterceptor[] _messageInterceptors;
-
-		public PrePostReceiveMessageHandlerDecorator(IEnumerable<IMessageInterceptor> messageInterceptors)
-		{
-			_messageInterceptors = messageInterceptors.ToArray();
-		}
-
 		public Action<SubscriberIdAndMessage, IServiceProvider, IMessageHandlerDecoratorChain> Accept =>
 			(subscriberIdAndMessage, serviceProvider, messageHandlerDecoratorChain) =>
 			{
 				IMessage message = subscriberIdAndMessage.Message;
-				PreReceive(message);
+				IMessageInterceptor[] messageInterceptors =
+					serviceProvider.GetServices<IMessageInterceptor>().ToArray();
+				PreReceive(message, messageInterceptors);
 				try
 				{
 					messageHandlerDecoratorChain.InvokeNext(subscriberIdAndMessage, serviceProvider);
 				}
 				finally
 				{
-					PostReceive(message);
+					PostReceive(message, messageInterceptors);
 				}
 			};
 
-		private void PreReceive(IMessage message)
+		private void PreReceive(IMessage message, IEnumerable<IMessageInterceptor> messageInterceptors)
 		{
-			foreach (IMessageInterceptor messageInterceptor in _messageInterceptors)
+			foreach (IMessageInterceptor messageInterceptor in messageInterceptors)
 			{
 				messageInterceptor.PreReceive(message);
 			}
 		}
 
 
-		private void PostReceive(IMessage message)
+		private void PostReceive(IMessage message, IEnumerable<IMessageInterceptor> messageInterceptors)
 		{
-			foreach (IMessageInterceptor messageInterceptor in _messageInterceptors)
+			foreach (IMessageInterceptor messageInterceptor in messageInterceptors)
 			{
 				messageInterceptor.PostReceive(message);
 			}
