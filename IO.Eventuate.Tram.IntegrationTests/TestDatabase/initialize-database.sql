30dd0e8c77e62c652cf0f1c3c90af5e1d3bb97ba
Doug Gish <doug.gish@ge.com>
2019-06-05 14:57:23 -0600

    Update sql script used for integration tests

--- IO.Eventuate.Tram.IntegrationTests/TestDatabase/initialize-database.sql
+++ IO.Eventuate.Tram.IntegrationTests/TestDatabase/initialize-database.sql
@@ -10,15 +10,25 @@ DROP TABLE IF EXISTS $(TRAM_SCHEMA).message;
 DROP TABLE IF EXISTS $(TRAM_SCHEMA).received_messages;
 
 CREATE TABLE $(TRAM_SCHEMA).message (
-  id NVARCHAR(200) PRIMARY KEY,
+  id VARCHAR(450) PRIMARY KEY,
   destination NVARCHAR(1000) NOT NULL,
   headers NVARCHAR(1000) NOT NULL,
   payload NVARCHAR(MAX) NOT NULL,
-  published SMALLINT DEFAULT 0
+  published SMALLINT DEFAULT 0,
+  creation_time BIGINT
 );
 
+-- ADD default value expression for creation_time
+ALTER TABLE $(TRAM_SCHEMA).message ADD DEFAULT DATEDIFF_BIG(ms, '1970-01-01 00:00:00', GETUTCDATE()) FOR creation_time
+
+CREATE INDEX message_published_idx ON $(TRAM_SCHEMA).message(published, id);
+
 CREATE TABLE $(TRAM_SCHEMA).received_messages (
-  consumer_id NVARCHAR(200),
-  message_id NVARCHAR(200),
-  PRIMARY KEY(consumer_id, message_id)
+  consumer_id VARCHAR(450),
+  message_id VARCHAR(450),
+  PRIMARY KEY(consumer_id, message_id),
+  creation_time BIGINT
 );
+
+-- ADD default value expression for creation_time
+ALTER TABLE $(TRAM_SCHEMA).received_messages ADD DEFAULT DATEDIFF_BIG(ms, '1970-01-01 00:00:00', GETUTCDATE()) FOR creation_time
