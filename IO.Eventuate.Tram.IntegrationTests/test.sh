cd86a73847e08360ad032e2041c1c8570992df67
Doug Gish <doug.gish@ge.com>
2020-04-14 11:09:55 -0600

    Add script to run integration tests

---
+++ IO.Eventuate.Tram.IntegrationTests/test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+docker-compose down
+docker-compose up -d mssql
+
+# Wait for MSSQL to start up
+sleep 40s
+
+docker-compose up --exit-code-from dbsetup dbsetup
+docker-compose up -d zookeeper
+docker-compose up -d kafka
+docker-compose up -d cdcservice1
+docker-compose up -d cdcservice2
+                            
+# Wait for dockers to start up
+sleep 40s
+
+docker-compose up --exit-code-from eventuatetramtests eventuatetramtests
+docker stats --no-stream --all
