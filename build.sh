310933909b9b8c6d998457b479031177178ccadf
Doug Gish <doug.gish@ge.com>
2020-04-20 16:40:39 -0600

    Try overriding PackageId and RepositoryUrl

--- build.sh
+++ build.sh
@@ -11,6 +11,8 @@ if [[ ${IS_PREVIEW} = true ]] ; then
     VERSION_PREFIX=preview-${GITHUB_RUN_NUMBER}
 fi
 
-dotnet build -c Release --version-suffix ${VERSION_PREFIX}
+PROPS="-p:PackageId=${PACKAGE_ID} -p:RepositoryUrl=https://github.com/${GITHUB_REPOSITORY}"
 
-dotnet pack -c Release --version-suffix ${VERSION_PREFIX}
+dotnet build -c Release --version-suffix ${VERSION_PREFIX} ${PROPS}
+
+dotnet pack -c Release --no-build --version-suffix ${VERSION_PREFIX} ${PROPS} IO.Eventuate.Tram
