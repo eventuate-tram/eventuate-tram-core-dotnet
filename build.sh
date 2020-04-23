c6a2f84af53ed0e80511c6bdefb7c7d930d71f95
Doug Gish <doug.gish@ge.com>
2020-04-23 15:49:15 -0600

    Publish "release" packages to a different nuget repo
    Pushed tags that start with 'v' are used to indicate a release build. The release build will use the version defined by the project file (it won't override the version suffix using the run number). Also, release nuget package is published to a different repository.

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 set -e
 set -x
 
-# Always a preview version for now
-IS_PREVIEW=true
-
-VERSION_PREFIX='""'
-if [[ ${IS_PREVIEW} = true ]] ; then
+VERSION_ARGS=()
+# If this is not a tagged release, set version suffix to 'preview-{run_number}'
+# Otherwise we will use the version as it is defined in the project file
+if [[ ${IS_TAGGED_RELEASE} != 'true' ]] ; then
     VERSION_SUFFIX=preview-${GITHUB_RUN_NUMBER:-local}
+    VERSION_ARGS=("--version-suffix" "${VERSION_SUFFIX}")
 fi
 
-dotnet build -c Release --version-suffix ${VERSION_SUFFIX}
+dotnet build -c Release ${VERSION_ARGS[@]}
 
-dotnet pack -c Release --no-build --version-suffix ${VERSION_SUFFIX} -p:PackageId=${PACKAGE_ID:-IO.Eventuate.Tram} IO.Eventuate.Tram
+dotnet pack -c Release --no-build ${VERSION_ARGS[@]} -p:PackageId=${PACKAGE_ID:-IO.Eventuate.Tram} IO.Eventuate.Tram
