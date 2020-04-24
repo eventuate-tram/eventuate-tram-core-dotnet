628e1dd5b659837b820ff240700243d358adb1a9
Doug Gish <doug.gish@ge.com>
2020-04-24 11:51:32 -0600

    Fix a typo in comments
    Co-Authored-By: Bert Zylstra <Bert.Zylstra@ge.com>

--- IO.Eventuate.Tram.IntegrationTests/TestHelpers/DynamicEventuateSchemaModelCacheKeyFactory.cs
+++ IO.Eventuate.Tram.IntegrationTests/TestHelpers/DynamicEventuateSchemaModelCacheKeyFactory.cs
@@ -6,7 +6,7 @@ namespace IO.Eventuate.Tram.IntegrationTests.TestHelpers
 {
 	/// <summary>
 	/// Used to ensure a new model is created for each different EventuateSchema
-	/// Otherwise, the first model created is cached and the used even if the EventuateSchema has changed
+	/// Otherwise, the first model created is cached and used even if the EventuateSchema has changed
 	/// </summary>
 	public class DynamicEventuateSchemaModelCacheKeyFactory : IModelCacheKeyFactory
 	{
