a4be9b0e9d2ac1cf3ea1551322b67c3fb9e49c62
Doug Gish <doug.gish@ge.com>
2025-03-14 16:06:24 -0600

    Use docker compose instead of docker-compose

--- IO.Eventuate.Tram.IntegrationTests/test.sh
+++ IO.Eventuate.Tram.IntegrationTests/test.sh
@@ -3,17 +3,17 @@
 set -e
 set -x
 
-docker-compose down
-docker-compose up -d mssql
+docker compose down
+docker compose up -d mssql
 
 # Wait for MSSQL to start up
 sleep 40s
 
-docker-compose run --rm kafka-setup
-docker-compose run --rm dbsetup
-docker-compose up -d zookeeper
-docker-compose up -d kafka
-docker-compose up -d cdcservice
+docker compose run --rm kafka-setup
+docker compose run --rm dbsetup
+docker compose up -d zookeeper
+docker compose up -d kafka
+docker compose up -d cdcservice
                             
 # Wait for docker containers to start up
 sleep 40s
@@ -23,7 +23,7 @@ docker ps
 docker stats --no-stream --all
 
 # Run tests
-docker-compose run --rm eventuatetramtests
+docker compose run --rm eventuatetramtests
 
 # Tear down test environment
-docker-compose down -v --remove-orphans
+docker compose down -v --remove-orphans
